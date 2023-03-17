b6de41213144ccaa64051feb7bd41b5835b5f714
Warley Dev <warleycm7@gmail.com>
2023-03-16 21:17:08 -0300

    Primeira forma de criar metodos com outros metodos de parametro

---
+++ src/application/Program.java
@@ -0,0 +1,46 @@
+package application;
+
+import entities.Contract;
+import entities.Installment;
+import model.services.ContractService;
+import model.services.PaypalService;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Entre os dados do contrato:");
+        System.out.print("Número: ");
+        int number = sc.nextInt();
+
+        System.out.print("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+
+        System.out.print("Valor do contrato: ");
+        double valueContract = sc.nextDouble();
+
+        Contract contract = new Contract(number, date, valueContract);
+
+        System.out.print("Entre com o numero de parcelas: ");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(contract, n);
+
+        System.out.println("PARCELAS: ");
+        for (Installment installment : contract.getInstallments()){
+            System.out.println(installment);
+        }
+
+        sc.close();
+    }
+}
